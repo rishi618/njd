4cdb9859eda2630025c6318c7589975797241f9f
rishi618 <rishiyogpeeth@gmail.com>
2025-01-16 16:47:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Handle surprise button click
+document.getElementById("surpriseButton").addEventListener("click", function() {
+    var surpriseMessage = document.getElementById("surpriseMessage");
+    surpriseMessage.classList.toggle("hidden"); // Show/hide the surprise message
+});
